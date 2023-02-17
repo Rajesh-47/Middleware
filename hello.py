6b2bfe4b5f667720d6a8739bfeaf14208b582557
Duggempudi Rajesh <duggempudi.rajesh@ibm.com>
2023-02-17 12:58:32 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+# print("hello");
+# a=10; print(a);
+# b=5.5; print(b);
+# username= input("enter name");
+# print(username);
+
+name = input( "What is your name: " )
+age = int( input( "How old are you: " ) )
+year = str( ( 2023 - age ) + 100 )
+print( name + " will be 100 years old in the year " + year )
